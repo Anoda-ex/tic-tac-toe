e632a18ca4806742e6d41a604df7d601f73ce6fe
Anoda-ex <sasha15200201@gmail.com>
2021-12-13 23:31:45 +0300

    build 1.0

--- github.bat
+++ github.bat
@@ -1,6 +1,6 @@
 git init
-git add .
 git checkout -b gh-pages
+git add .
 git commit -m "build 1.0"
 git remote add origin https://github.com/Anoda-ex/tic-tac-toe.git
-git push -u origin gh-page
+git push -u origin gh-pages
